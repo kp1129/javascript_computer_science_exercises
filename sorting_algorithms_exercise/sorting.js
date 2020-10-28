e1a21eebd336887601e780deba386238415da909
kp1129 <katya.p.1129@gmail.com>
2020-10-27 22:52:34 -0700

    implement sorting algos in js

--- sorting_algorithms_exercise/sorting.js
+++ sorting_algorithms_exercise/sorting.js
@@ -29,9 +29,48 @@ console.log(bubbleSort([9,-2,0,35,4,-10,22,12])) //[-10,-2,0,4,9,12,22,35]
 
 // Implement selection sort. Your function should 
 // accept an array and return an array of sorted values.
+const selectionSort = (arr) => {
 
+    for (let i = 0; i < arr.length - 1; i++){
+        let currentIx = i;
+        let smallestIx = currentIx;
 
+        for (let j = currentIx; j < arr.length; j++){
+            if (arr[j] < arr[smallestIx]){
+                smallestIx = j;
+            }
+        }
+        temp = arr[currentIx];
+        arr[currentIx] = arr[smallestIx];
+        arr[smallestIx] = temp;
+    }
+ 
+    return arr;
+}
+
+// test cases
+console.log('selectionSort');
+console.log(selectionSort([4,5,1,21,2,9,10,15])) // [1,2,4,5,9,10,15,21]
+console.log(selectionSort([9,-2,0,35,4,-10,22,12])) //[-10,-2,0,4,9,12,22,35]
 
 
 // Implement insertion sort. Your function should 
 // accept an array and return an array of sorted values.
+
+const insertionSort = (arr) => {
+    for (let i = 1; i < arr.length; i++){
+        let item = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > item){
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = item;
+    }
+    return arr;
+}
+
+// test cases
+console.log('insertionSort');
+console.log(insertionSort([4,5,1,21,2,9,10,15])) // [1,2,4,5,9,10,15,21]
+console.log(insertionSort([9,-2,0,35,4,-10,22,12])) //[-10,-2,0,4,9,12,22,35]
